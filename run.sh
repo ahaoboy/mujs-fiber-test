884921cac1fd360e3e48aff935edc38d68431a03
ahaoboy <504595380@qq.com>
2024-08-12 06:52:12 +0800

    add boa

--- run.sh
+++ run.sh
@@ -3,5 +3,10 @@
 mujs es5/index.js > docs/mujs.json
 qjs es5/index.js > docs/qjs.json
 llrt es5/index.js > docs/llrt.json
+
+# TODO: boa only run es6
+# https://github.com/boa-dev/boa/issues/3518
+boa dist/index.js > docs/boa.json
+
 # node es5/index.js > assets/node.json
 # tjs run es5/index.js > tjs.json
